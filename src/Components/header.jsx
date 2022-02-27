30103866a0f5968a502194547affa1099cc3cc46
Shaleen <coolshaleen1@gmail.com>
2022-02-27 18:35:10 +0530

    fixed header image

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,19 +3,21 @@ import { Box } from "@mui/material";
 import headerMobile from "../images/bg-header-mobile.svg";
 import headerDesktop from "../images/bg-header-desktop.svg";
 const Header = () => {
-    return (
-      <Box
-        sx={{
-          backgroundImage: {
-            xs: `url(${headerMobile})`,
-            lg: `url(${headerDesktop})`,
-          },
-          height: "150px",
-          borderBottom: "3px solid hsl(180, 29%, 50%)",
-          overflow:'hidden'
-        }}
-      />
-    );
+  return (
+    <Box
+      sx={{
+        backgroundImage: {
+          xs: `url(${headerMobile})`,
+          md: `url(${headerDesktop})`,
+          lg: `url(${headerDesktop})`,
+        },
+        height: "150px",
+        borderBottom: "3px solid hsl(180, 29%, 50%)",
+        backgroundPosition: "center",
+        overflow: "hidden",
+      }}
+    />
+  );
 };
 
 export default Header;
